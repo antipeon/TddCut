d406df11e1cf14ac79c5270e92b93bc78d414f52
Samat Gainutdinov <s.gainutdinov@dodobrands.io>
2025-11-20 09:22:06 +0300

    Initial Commit

---
+++ TddCut/TddCutApp.swift
@@ -0,0 +1,17 @@
+//
+//  TddCutApp.swift
+//  TddCut
+//
+//  Created by samat on 20.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct TddCutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TddCutTests/TddCutTests.swift
@@ -0,0 +1,17 @@
+//
+//  TddCutTests.swift
+//  TddCutTests
+//
+//  Created by samat on 20.11.2025.
+//
+
+import Testing
+@testable import TddCut
+
+struct TddCutTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
